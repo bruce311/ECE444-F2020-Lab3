6eae1cd0726056899a47101f994f3a73f5c4d5c6
bruce hou <bruce@bruces-MacBook-Pro.local>
2020-10-02 19:13:52 -0400

    first commit for activity2

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 this repo is a clone of
 https://github.com/miguelgrinberg/flasky
 
+Activity 1:
 ![Activity1](https://github.com/bruce311/ECE444-F2020-Lab3/blob/main/screenshots/Screen%20Shot%202020-10-02%20at%205.32.03%20PM.png)
--- hello.py
+++ hello.py
@@ -2,10 +2,24 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required, ValidationError
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+
+def validate_email(form, field):
+    if '@' not in field.data:
+        raise ValidationError("Please include an '@' in the email address. '{}' is missing an '@'.".format(field.data))
+
+class InfoForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    email = StringField('What is your UofT Email address?', validators=[Required(), validate_email])
+    submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -15,14 +29,22 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time = datetime.utcnow())
+    name = None
+    email = None
+    form = InfoForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        email = form.email.data
+        form.email.data = ''
+    return render_template('index.html', form=form, name=name, email=email)
+
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name, current_time=datetime.utcnow())
-
 
 
 if __name__ == '__main__':
